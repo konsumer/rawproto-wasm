8c0ed8bbb3e5191833787c07b163f75792f73b28
Simon Warta <simon@warta.it>
2022-02-11 12:45:28 +0100

    Use decode_raw as library

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,9 @@
 // code mostly came from // this is from https://github.com/confio/decode_raw
 // with a little wasm wrapping
 
+use decode_raw::{try_parse_entries, Entry, EntryValue, ParseConfig};
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
-use protofish::decode::UnknownValue;
-use protofish::prelude::*;
-use protofish::prelude::{Context, FieldValue};
-use serde::{Serialize, Deserialize};
 
 #[wasm_bindgen]
 extern "C" {
@@ -18,30 +16,41 @@ extern "C" {
 #[wasm_bindgen]
 pub fn parse_raw(bytes: &[u8], js_path: &str, js_config: &JsValue) -> JsValue {
     console_error_panic_hook::set_once();
-    let config = js_config.into_serde().unwrap();
+
+    let config: SerdeParseConfig = js_config.into_serde().unwrap();
     let path = parse_select_query(js_path);
-    let mut ret: Vec<Entry> = vec![];
-    if let Some(entries) = try_parse_entries(bytes, &[], config) {
+    let mut ret: Vec<SerdeEntry> = vec![];
+
+    if let Some(entries) = try_parse_entries(bytes, &[], config.into()) {
         for entry in entries {
             if !entry.path.starts_with(&path) {
                 continue;
             }
-            ret.push(entry);
+            ret.push(entry.into());
         }
     }
+
     let js_ret = JsValue::from_serde(&ret).unwrap();
     js_ret
 }
 
-
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
-pub struct Entry {
+pub struct SerdeEntry {
     pub path: Vec<u64>,
-    pub value: EntryValue,
+    pub value: SerdeEntryValue,
+}
+
+impl From<Entry> for SerdeEntry {
+    fn from(source: Entry) -> SerdeEntry {
+        SerdeEntry {
+            path: source.path,
+            value: source.value.into(),
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
-pub enum EntryValue {
+pub enum SerdeEntryValue {
     Int(u128),
     /// Wire type 2 (length delimited).
     Bytes(Vec<u8>),
@@ -49,17 +58,28 @@ pub enum EntryValue {
     CloseNested,
 }
 
+impl From<EntryValue> for SerdeEntryValue {
+    fn from(source: EntryValue) -> SerdeEntryValue {
+        match source {
+            EntryValue::Int(v) => SerdeEntryValue::Int(v),
+            EntryValue::Bytes(v) => SerdeEntryValue::Bytes(v),
+            EntryValue::OpenNested => SerdeEntryValue::OpenNested,
+            EntryValue::CloseNested => SerdeEntryValue::CloseNested,
+        }
+    }
+}
+
 #[derive(Debug, Copy, Clone, Serialize, Deserialize)]
-pub struct ParseConfig {
+pub struct SerdeParseConfig {
     pub no_fixed64: bool,
     pub no_fixed32: bool,
 }
 
-impl Default for ParseConfig {
-    fn default() -> Self {
-        Self {
-            no_fixed64: false,
-            no_fixed32: false,
+impl From<SerdeParseConfig> for ParseConfig {
+    fn from(source: SerdeParseConfig) -> ParseConfig {
+        ParseConfig {
+            no_fixed64: source.no_fixed64,
+            no_fixed32: source.no_fixed32,
         }
     }
 }
@@ -78,153 +98,8 @@ pub fn parse_select_query(input: &str) -> Vec<u64> {
     path
 }
 
-/// Tries to parse bytes as protobuf message and returns entries.
-/// Each entry represents one line in the output.
-pub fn try_parse_entries(bytes: &[u8], path: &[u64], config: ParseConfig) -> Option<Vec<Entry>> {
-    if bytes.is_empty() {
-        // Empty byte arrays should be represented as "" instead of empty message
-        return None;
-    }
-
-    let fields = decode_fields(&bytes);
-    let mut out = Vec::<Entry>::new();
-    for field in fields.into_iter() {
-        let mut nested_path = path.to_vec();
-        nested_path.push(field.number);
-
-        match &field.value {
-            Value::Unknown(unknown) => match unknown {
-                UnknownValue::Fixed64(v) => {
-                    if config.no_fixed64 {
-                        return None;
-                    }
-                    out.push(Entry {
-                        path: nested_path,
-                        value: EntryValue::Int((*v).into()),
-                    })
-                }
-                UnknownValue::Fixed32(v) => {
-                    if config.no_fixed32 {
-                        return None;
-                    }
-                    out.push(Entry {
-                        path: nested_path,
-                        value: EntryValue::Int((*v).into()),
-                    })
-                }
-                UnknownValue::Varint(v) => out.push(Entry {
-                    path: nested_path,
-                    value: EntryValue::Int(*v),
-                }),
-                UnknownValue::VariableLength(v) => {
-                    if let Some(nested_entries) = try_parse_entries(&v, &nested_path, config) {
-                        out.push(Entry {
-                            path: nested_path.clone(),
-                            value: EntryValue::OpenNested,
-                        });
-                        out.extend(nested_entries);
-                        out.push(Entry {
-                            path: nested_path,
-                            value: EntryValue::CloseNested,
-                        });
-                    } else {
-                        out.push(Entry {
-                            path: nested_path,
-                            value: EntryValue::Bytes(v.to_vec()),
-                        })
-                    }
-                }
-                UnknownValue::Invalid(_wire_type, _bytes) => {
-                    return None;
-                }
-            },
-            _ => return None,
-        };
-    }
-    Some(out)
-}
-
-// TODO: allow param for proto, so we can do partial parse
-pub fn decode_fields(bytes: &[u8]) -> Vec<FieldValue> {
-    let context = Context::parse(&[r#"
-        syntax = "proto3";
-        package Proto;
-
-        message Empty { }
-    "#])
-    .unwrap();
-
-    let request = context.get_message("Proto.Empty").unwrap();
-    let value = request.decode(bytes, &context);
-    value.fields
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    #[test]
-    fn try_parse_entries_works() {
-        // one
-        let entries = try_parse_entries(b"\x12\x07Unknown", &[], ParseConfig::default()).unwrap();
-        assert_eq!(
-            entries,
-            &[Entry {
-                path: vec![2],
-                value: EntryValue::Bytes(b"Unknown".to_vec())
-            }]
-        );
-
-        // two
-        let entries = try_parse_entries(
-            b"\x12\x07Unknown\x12\x07Unknown",
-            &[],
-            ParseConfig::default(),
-        )
-        .unwrap();
-        assert_eq!(
-            entries,
-            &[
-                Entry {
-                    path: vec![2],
-                    value: EntryValue::Bytes(b"Unknown".to_vec())
-                },
-                Entry {
-                    path: vec![2],
-                    value: EntryValue::Bytes(b"Unknown".to_vec())
-                }
-            ]
-        );
-
-        // nested path
-        let entries = try_parse_entries(b"\x12\x07Unknown", &[42], ParseConfig::default()).unwrap();
-        assert_eq!(
-            entries,
-            &[Entry {
-                path: vec![42, 2],
-                value: EntryValue::Bytes(b"Unknown".to_vec())
-            }]
-        );
-
-        // No valid protobuf (incomplete)
-        let res = try_parse_entries(
-            b"\x12\x07Unknown\x0a\x0fAtlantic ",
-            &[],
-            ParseConfig::default(),
-        );
-        assert_eq!(res, None);
-
-        // No valid protobuf (wrong wire type)
-        // End group (deprecated) in field 2: hex((2 << 3) | 4)
-        let res = try_parse_entries(b"\x14\x07Unknown", &[], ParseConfig::default());
-        assert_eq!(res, None);
-    }
-
-    #[test]
-    fn try_parse_entries_returns_none_for_empty() {
-        let res = try_parse_entries(b"", &[], ParseConfig::default());
-        assert_eq!(res, None);
-    }
 
     // TODO: not sure why, but these throw type annotations needed error
 
@@ -247,4 +122,3 @@ mod tests {
     //     assert_eq!(parse_select_query("3.3"), &[3, 3]);
     // }
 }
-
