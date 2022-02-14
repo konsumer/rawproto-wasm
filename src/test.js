1542989d13bd41bacef7cfac5cb820e08e1ed5af
David Konsumer <konsumer@jetboystudio.com>
2022-02-14 14:50:07 -0800

    simpler test, example string code

--- src/test.js
+++ src/test.js
@@ -1,7 +1,20 @@
 const { parse_raw } = require('../pkg/rawproto.js')
 
-test('rawproto', () => {
-  const opt = { no_fixed64: true, no_fixed32: false }
-  expect(parse_raw(Buffer.from("\x12\x07Unknown"), '', opt)).toMatchSnapshot()
-  expect(parse_raw(Buffer.from("\x12\x07Unknown\x12\x07Unknown"), '', opt)).toMatchSnapshot()
+function getString (binary, path) {
+  const entries = parse_raw(binary, path, { no_fixed64: true, no_fixed32: true })
+  return entries.map(entry => {
+    return String.fromCharCode(...entry.value.Bytes)
+  })
+}
+
+describe('rawproto', () => {
+  test('basic parsing', () => {
+    const opt = { no_fixed64: true, no_fixed32: false }
+    expect(parse_raw(Buffer.from('\x12\x07Unknown'), '', opt)).toMatchSnapshot()
+    expect(parse_raw(Buffer.from('\x12\x07Unknown\x12\x07Unknown'), '', opt)).toMatchSnapshot()
+  })
+
+  test('string parsing', () => {
+    expect(getString(Buffer.from('\x12\x07Unknown\x12\x07Unknown'), '.2')).toMatchSnapshot()
+  })
 })
