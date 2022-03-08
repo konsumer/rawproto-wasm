7bb319b86b09654cafca3dbed2e05cc996a240b9
David Konsumer <dkonsumer@gummicube.com>
2022-03-08 13:48:14 -0800

    remove wasm RC

--- src/lib.js
+++ src/lib.js
@@ -5,18 +5,16 @@ import wasmBytes from '../target/web/wasm.js'
 
 let r
 
-async function sideeffect () {
-   r = await init(wasmBytes)
-}
-sideeffect()
-
 // wrap parse_raw with optional params
 export function parseRaw(bin, path='.', cfg = { no_fixed64: false, no_fixed32: false }) {
+  if (!r) {
+     r = await init(wasmBytes)
+  }
   return r.parse_raw(bin, path, cfg)
 }
 
 // get a string from a path
-export function getString (binary, path) {
+export async function getString (binary, path) {
   const entries = parseRaw(binary, path)
   return entries.map(entry => {
     return String.fromCharCode(...entry.value.Bytes)
